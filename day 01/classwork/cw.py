d3393a1d35262eca969762f49961505b6d9dc0a7
qura2801 <data35657@gmail.com>
2025-10-02 05:59:58 -0700

    second commit

---
+++ day 01/classwork/cw.py
@@ -0,0 +1,5 @@
+name1="xvicha"
+print('name1')
+
+name2="iako"
+print("name2")
---
+++ day 01/homework/hw.py
@@ -0,0 +1,59 @@
+#hello world
+ 
+symbol1 = "%"
+
+symbol2 = "%%"
+
+symbol3 = "%%%"
+
+symbol4 = "%%%%"
+
+symbol5 = "%%%%%"
+
+print(symbol1)
+print(symbol2)
+print(symbol3)
+print(symbol4)
+print(symbol5)
+
+food1 = "xinkali"
+food2 = "mwvadi"
+food3 = "qababi"
+food4 = "shaurma"
+food5 = "xachapuri"
+
+print (food1)
+print (food2)
+print (food3)
+print (food4)
+print (food5)
+
+print("hello data")
+
+symbol1 = "&&&&&&&&&"
+symbol2 = "&&Hello&&"
+symbol3 = "&&&&&&&&&"
+
+print (symbol1)
+print (symbol2)
+print (symbol3)
+
+food1 = 'xachapuri'
+price1 = 11.29
+print(food1, price1)
+
+food2 = "shaurma" 
+price2 = 10.99
+print(food2, price2)
+
+food3 = "mwvadi"
+price3 = 8
+print(food3, price3)
+
+food4 = "qababi"
+price4 = 6.78
+print(food4, price4)
+
+food5 = "xinkali"
+price5 = 1.60
+print(food5, price5)
