899a7b7d48929840fd9d9f09b0d419040835f8df
qura2801 <data35657@gmail.com>
2025-10-07 07:15:57 -0700

    second commit

---
+++ day02/homework/hw02.py
@@ -0,0 +1,7 @@
+#hello world
+
+print("HELLO EVERYONE")
+print('we talk about HTML, CSS and JAVA SCRIPT')
+print("html is where will the drawing be the title image or something like that")
+print("css is style decoration layout animations text colors that how people can see")
+print("java script is brain that is when you press the button something happens Open close work the games")
