5c8a9c750be00a25ba47cfbbb2cd751bcda62d6c
qura2801 <data35657@gmail.com>
2025-10-09 06:14:00 -0700

    second commit

---
+++ day03/classwork/day03.py
@@ -0,0 +1,11 @@
+food1=input("whats your favorite food:")
+food2=input("whats your favorite food:")
+food3=input("whats your favorite food:")
+food4=input("whats your favorite food:")
+
+print1=food1
+print2=food2
+print3=food3
+print4=food4
+
+
---
+++ day03/homework/day03.py
@@ -0,0 +1,11 @@
+name=input('enter your name:')
+print(name)
+
+age=int(input("enter your age"))
+print(age)
+
+food=input("your favorite food mec miyvars:")
+print(food)
+
+name1=input("whats your name")
+print("name1")
